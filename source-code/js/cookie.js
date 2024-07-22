4fcd1afe192fd24581c6e5321cfca4bbd410a60d
shixin.guo <shixin.guo@zoom.us>
2024-07-22 13:05:51 -0700

    feat: clean

--- source-code/js/cookie.js
+++
@@ -1,5 +0,0 @@
-
-export function getCookie(name: string): string | undefined {
-  const match = document.cookie.match(new RegExp("(^| )" + name + "=([^;]+)"));
-  if (match) return match[2];
-}
--- source-code/js/env.js
+++
@@ -1,8 +0,0 @@
-
-const ua: string = navigator.userAgent.toLowerCase();
-
-export const isEdge: boolean = /edge\/(\d+)/.test(ua);
-export const isIELower: boolean =
-  ua.indexOf("compatible") > -1 && ua.indexOf("msie") > -1;
-export const isIE11: boolean = ua.indexOf("trident") > -1 && ua.indexOf("rv:11.0") > -1;
-export const isIpadOS: boolean = ua.includes("mac") && "ontouchend" in document;
